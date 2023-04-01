82d26dee30371be23799c05025608b7262dfe83a
erenuzmez <erenuzmezz@gmail.com>
2023-04-02 02:11:06 +0300

    Create Odev18CiftSayiToplama.java

---
+++ Odev18CiftSayiToplama.java
@@ -0,0 +1,16 @@
+package CDonguler;
+import java.util.Scanner;
+public class Odev18CiftSayiToplama {
+    public static void main(String[] args) {
+        int number,total=0;
+        Scanner input=new Scanner(System.in);
+        do {
+            System.out.println("Sayı giriniz:");
+            number=input.nextInt();
+            if (number%4==0){
+                total=total+number;
+            }
+        }while (number%2==0);
+        System.out.println("Çift ve dördün katı sayıların toplamı:"+total);
+    }
+}
